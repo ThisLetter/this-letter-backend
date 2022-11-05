afc7c14d9b939079cd2c5473bb5b7aac57bcfc5a
cesdea <aprkvnem@gmail.com>
2022-11-05 18:12:04 +0900

    add 환경변수, 데이터베이스

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { LetterController } from './letter/letter.controller';
 import { GroupController } from './group/group.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+const ENV=process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: (ENV.NODE_ENV === 'production') ? '.production.env' : '.development.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: ENV.DATABASE_HOST,
+      port: ENV.DATABASE_PORT as unknown as number,
+      username: ENV.DATABASE_USERNAME,
+      password: ENV.DATABASE_PASSWORD,
+      database: ENV.DATABASE,
+      entities: [],
+      synchronize: ENV.SYNCHROIZE as unknown as boolean,
+    }),
+  ],
   controllers: [AppController, UserController, LetterController, GroupController],
   providers: [AppService],
 })
